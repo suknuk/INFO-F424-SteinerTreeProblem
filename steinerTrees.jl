46afd534b49ef1fbb9f418f64f699923a96a9f39
Raymond Lochner <ray_lochner@yahoo.de>
2017-05-30 15:57:21 +0200

    Starting flow formulation PF

--- steinerTrees.jl
+++ steinerTrees.jl
@@ -24,11 +24,25 @@ println("Nodes $(nodes), edges $(edges), terminals $(length(terminals))")
 #Binary variable to indicate if edge between nodes i and j was selected
 @variable(m, x[1:nodes, 1:nodes], Bin)
 
+#Quantity of commodity t flowing through edge i to j
+@variable(m, yt[1:nodes, 1:nodes], Int)
+
 
 #############
 # Constraints
 #############
 
+###
+# Keeping the similarity in the Binary Matrix
+# Not sure to let it?
+#=
+for i = 1:nodes
+	for j = 1:nodes
+		@constraint(m, x[i,j] == x[j,i])
+	end
+end=#
+
+###
 # Every terminal has to be selected
 # Formulation P_{UC}
 # We could also do 'x[t,;] >= 1'
@@ -36,12 +50,29 @@ for t in terminals
 	@constraint(m, sum(x[:,t]) >= 1)
 end
 
-# Keeping the similarity in the Binary Matrix
+###
+# Multicommodity flow constraints
 for i = 1:nodes
 	for j = 1:nodes
-		@constraint(m, x[i,j] == x[j,i])
+		###
+		# Constraint 3.1
+		# TODO
+		decision = 0
+		if in(i, terminals)
+			decision = 1
+		end
+		@constraint(m, yt[j,i] - yt[i,j] == decision)
+		
+		###
+		# Constraint 3.2
+		@constraint(m, x[i,j] >= yt[i,j])
+
+		###
+		# Constraint 3.3
+		@constraint(m, yt[i,j] >= 0)
 	end
 end
+
 
 ###########
 # Objective
@@ -55,8 +86,8 @@ function objectiveFunction()
 			total += x[i,j] * adjMatrix[i,j]
 		end
 	end
-	# Divide by two because we have a Adjacency Matrix
-	total = total / 2
+	# Divide by two because we have a Adjacency Matrix -> maybe?
+	# total = total / 2
 	return total
 end
 
