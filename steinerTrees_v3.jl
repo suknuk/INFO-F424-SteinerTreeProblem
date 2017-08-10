4c67ef09dc5f48746b64302e59f0e36a51e49ddc
Raymond Lochner <ray_lochner@yahoo.de>
2017-08-10 11:47:59 +0200

    Working 3rd formulation

--- steinerTrees_v3.jl
+++ steinerTrees_v3.jl
@@ -41,98 +41,96 @@ R1 = terminals[2:end]
 #############
 
 ###
-# Constraints 4.1 - 4.3
-for k = 1:length(terminals)
-	for l = 1:length(terminals)
-		#if k == l
-		#	continue
-		#end
-		zk = terminals[k]
-		zl = terminals[l]
-		if in(zk, R1) && in(zl, R1)# && k != l	
-			for i = 1:nodes
-				incomingFlow41 = outgoingFlow41 = 0
-				incomingFlow42 = outgoingFlow42 = 0
-				incomingFlow43 = outgoingFlow43 = 0
-				for j = 1:nodes
-					if i != j && adjMatrix[i,j] != typemax(Int32)
-						###
-						# Constraint 4.1 flow
-						incomingFlow41 += yhat[j,i,k,l]
-						outgoingFlow41 += yhat[i,j,k,l]
-						
-						###
-						# Constraint 4.2 flow
-						incomingFlow42 += yhat[j,i,k,l] + yhat_left[j,i,k,l]
-						outgoingFlow42 += yhat[i,j,k,l] + yhat_left[i,j,k,l]
-						
-						###
-						# Constraint 4.3 flow
-						incomingFlow43 += yhat[j,i,k,l] + yhat_right[j,i,k,l]
-						outgoingFlow43 += yhat[i,j,k,l] + yhat_right[i,j,k,l]
-					end
-				end
-			
-				###
-				# Constraint 4.1
-				if i == z1
-					@constraint(m, incomingFlow41 - outgoingFlow41 >= -1)
-				else
-					@constraint(m, incomingFlow41 - outgoingFlow41 == 0)
-				end
+# Constraint 1
+for t = 1:length(terminals)
+	zt = terminals[t]
+	for i = 1:nodes
+		incomingFlow = 0
+		outgoingFlow = 0
+		for j = 1:nodes
+			if i != j && adjMatrix[i,j] != typemax(Int32)
+				incomingFlow += y[j,i,t]
+				outgoingFlow += y[i,j,t]
+			end
+		end
 
-				###
-				# Constraint 4.2
-				if i == zk
-					@constraint(m, incomingFlow42 - outgoingFlow42 == 1)
-				elseif (i != z1) && (i != zk)
-					@constraint(m, incomingFlow42 - outgoingFlow42 == 0)
-				end
-				
-				###
-				# Constraint 4.3
-				if i == zl
-					@constraint(m, incomingFlow43 - outgoingFlow43 == 1)
-				elseif (i != z1) && (i != zl)
-					@constraint(m, incomingFlow43 - outgoingFlow43 == 0)
-				end
-				
-				
+		if in(zt, R1)
+			if i == zt
+				@constraint(m, incomingFlow - outgoingFlow == 1)
+			elseif i != z1 && i != zt
+				@constraint(m, incomingFlow - outgoingFlow == 0)
 			end
 		end
 	end
 end
 
 ###
-# Constraints 4.4 and 4.5
+# Constraint 2
 for k = 1:length(terminals)
 	for l = 1:length(terminals)
-		#if k == l
-		#	continue
-		#end
+		if k == l
+			continue
+		end
 		zk = terminals[k]
 		zl = terminals[l]
-		if in(zk, R1) && in(zl, R1)# && (k != l)
+		if in(zk, R1) && in(zl, R1)
 			for i = 1:nodes
+				incomingFlow = 0
+				outgoingFlow = 0
 				for j = 1:nodes
-					if (adjMatrix[i,j] == typemax(Int32)) || (i == j)
-						continue
+					if (adjMatrix[i,j] != typemax(Int32)) && (i != j)
+						incomingFlow += yhat[j,i,k,l]
+						outgoingFlow += yhat[i,j,k,l]
 					end
-					###
-					# Constraint 4.4
-					y_flow = yhat[i,j,k,l] + yhat_left[i,j,k,l] + yhat_right[i,j,k,l]
-					@constraint(m, y_flow <= x[i,j])
-					
-					###
-					# Constraint 4.5
-					@constraint(m, yhat[i,j,k,l] >= 0)
-					@constraint(m, yhat_left[i,j,k,l] >= 0)
-					@constraint(m, yhat_right[i,j,k,l] >= 0)
 				end
+				
+				if i == z1
+					@constraint(m, incomingFlow - outgoingFlow >= -1)
+				else
+					@constraint(m, incomingFlow - outgoingFlow >= 0)
+				end
+					
 			end
 		end
 	end
 end
+
+###
+# Constraints 3,4,5
+for i = 1:nodes
+	for j = 1:nodes
+		if i == j || adjMatrix[i,j] == typemax(Int32)
+			continue
+		end
+
+		for k = 1:length(terminals)
+			for l = 1:length(terminals)
+				if k == l 
+					continue
+				end
+				zk = terminals[k]
+				zl = terminals[l]
+				if in(zk,R1) && in(zl,R1)
+					
+					###
+					# Constraint 3
+					@constraint(m, yhat[i,j,k,l] <= y[i,j,k])
+					
+					###
+					# Constraint 4
+					@constraint(m, yhat[i,j,k,l] <= y[i,j,l])
+					
+					###
+					# Constraint 5
+					@constraint(m, y[i,j,k] + y[i,j,l] - yhat[i,j,k,l] <= x[i,j])
+				end
+			end
+		end
+
+	end
+end
+
+
 
 ###
 # Constraint 6
@@ -142,8 +140,10 @@ for i = 1:nodes
 	end
 	outgoing = incoming = 0
 	for j = 1:nodes
-		incoming += x[j,i]
-		outgoing += x[i,j]
+		if i != j && adjMatrix[i,j] != typemax(Int32)
+			incoming += x[j,i]
+			outgoing += x[i,j]
+		end
 	end
 	@constraint(m, incoming - outgoing <= 0)
 end
@@ -153,15 +153,18 @@ end
 # Constraint 7
 for i = 1:nodes
 	for j = 1:nodes
-		if i == j
+		if i == j || adjMatrix[i,j] == typemax(Int32)
 			continue
 		end
 
 		for k = 1:length(terminals)
+			zk = terminals[k]
 			for l = 1:length(terminals)
-				zk = terminals[k]
+				if k == l
+					continue
+				end
 				zl = terminals[l]
-				if in(zk,R1) && in(zl,R1) && (k != l)
+				if in(zk,R1) && in(zl,R1)
 					@constraint(m, yhat[i,j,k,l] >= 0)
 				end
 			end
@@ -205,7 +208,6 @@ if status == :Infeasible
 	error("No solution found!")
 else
 	println("Objective value: ", getobjectivevalue(m))
-	println(getvalue(x))
 end
 
 println("Solver took $timeTaken seconds to complete.")
